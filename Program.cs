70f790f4becb342882f61365a87edc601adf64bd
Tharun <thetharun52@gmail.com>
2025-04-30 16:51:37 +0530

    added delete and edit features

--- Program.cs
+++ Program.cs
@@ -77,12 +77,12 @@ app.MapGet("/api/movies", async (MovieContext db, HttpContext http) =>
         movie.User_Rating = Math.Round(movie.User_Rating, 2);
     }
 
-    var movieCards = GenerateMovieCards(movies);
+    var movieCards = GenerateMovieCards(movies, http.Session.GetString("isAdmin"));
 
     return Results.Content(movieCards, "text/html");
 });
 
-static string GenerateMovieCards(List<Movie> movies)
+static string GenerateMovieCards(List<Movie> movies, string isAdmin = "false")
 {
     if (movies.Count == 0)
     {
@@ -90,21 +90,35 @@ static string GenerateMovieCards(List<Movie> movies)
     }
 
     return string.Join("", movies.Select(movie => $@"
-        <div class='col-md-4 mb-4'>
+        <div class='col'>
             <div class='card movie-card h-100'>
                 <img src='{movie.Poster_Path}' class='card-img-top' alt='{movie.Title}'>
                 <div class='card-body'>
                     <h5 class='card-title'>{movie.Title}</h5>
                     <p class='card-text'>{movie.Overview}</p>
-                    <a href='/edit/{movie.Id}' id='editbutton' class='btn btn-primary'>Edit</a>
                 </div>
                 <div class='card-footer'>
                     <small class='text-muted'>Rating: {movie.User_Rating} | Language: {movie.Language} | Released: {movie.Release_Date}</small>
+
+                    {(isAdmin == "true" ? $@"
+                        <div class='mt-2 d-flex justify-content-between'>
+                            <form method='post' action='/deletemovie' onsubmit='return confirm(""Are you sure you want to delete this movie?"")'>
+                                <input type='hidden' name='id' value='{movie.Id}' />
+                                <button type='submit' class='btn btn-danger btn-sm'>Delete</button>
+                            </form>
+                            <form method='get' action='/editmovie'>
+                                <input type='hidden' name='id' value='{movie.Id}' />
+                                <button type='submit' class='btn btn-warning btn-sm'>Edit</button>
+                            </form>
+                        </div>
+                    " : "")}
                 </div>
             </div>
         </div>
     "));
 }
+
+
 
 
 // login page Route
@@ -144,13 +158,111 @@ app.MapGet("/adminindex", async (MovieContext db, HttpContext context) =>
 
         var adminMovieCards = GenerateMovieCards(movies); // See Step 3
         var finalHtml = template.Replace("{{MOVIES}}", adminMovieCards).Replace("{{SEARCH_TERM}}", "");
-
         
         return Results.Content(finalHtml, "text/html");
     }
     else
     {
         return Results.Redirect("/adminlogin");
+    }
+});
+
+app.MapPost("/deletemovie", async (MovieContext db, HttpContext context) =>
+{
+    if (context.Session.GetString("isAdmin") != "true")
+    {
+        context.Response.Redirect("/adminlogin");
+        return;
+    }
+
+    var form = await context.Request.ReadFormAsync();
+    var id = int.Parse(form["id"]);
+
+    var movie = await db.Movies.FindAsync(id);
+    if (movie != null)
+    {
+        db.Movies.Remove(movie);
+        await db.SaveChangesAsync();
+
+        var alert = $"<script>alert('Movie \"{movie.Title}\" has been deleted.'); window.location='/adminindex';</script>";
+        await context.Response.WriteAsync(alert);
+    }
+    else
+    {
+        var alert = "<script>alert('Movie not found.'); window.location='/adminindex';</script>";
+        await context.Response.WriteAsync(alert);
+    }
+});
+
+app.MapGet("/editmovie", async (MovieContext db, HttpContext context) =>
+{
+    if (context.Session.GetString("isAdmin") == "true")
+    {
+        var id = context.Request.Query["id"];
+        if (int.TryParse(id, out int movieId))
+        {
+            var movie = await db.Movies.FindAsync(movieId);
+            if (movie != null)
+            {
+                var html = await File.ReadAllTextAsync("wwwroot/editmovie.html");
+                html = html.Replace("{{MOVIE_ID}}", movie.Id.ToString())
+                           .Replace("{{MOVIE_TITLE}}", movie.Title)
+                           .Replace("{{MOVIE_OVERVIEW}}", movie.Overview)
+                           .Replace("{{MOVIE_RATING}}", movie.User_Rating.ToString())
+                           .Replace("{{MOVIE_LANGUAGE}}", movie.Language)
+                           .Replace("{{MOVIE_RELEASEDATE}}", movie.Release_Date);
+                await context.Response.WriteAsync(html);
+            }
+            else
+            {
+                context.Response.Redirect("/adminindex");
+            }
+        }
+        else
+        {
+            context.Response.Redirect("/adminindex");
+        }
+    }
+    else
+    {
+        context.Response.Redirect("/adminlogin");
+    }
+});
+
+app.MapPost("/editmovie", async (MovieContext db, HttpContext context) =>
+{
+    if (context.Session.GetString("isAdmin") != "true")
+    {
+        context.Response.Redirect("/adminlogin");
+        return;
+    }
+
+    var form = await context.Request.ReadFormAsync();
+    var movieId = int.Parse(form["id"]);
+    var title = form["title"];
+    var overview = form["overview"];
+    var rating = float.Parse(form["rating"]);
+    var language = form["language"];
+    
+    // Convert the release date from the form into a string
+    var releaseDate = form["releaseDate"];  // this is a string in "yyyy-MM-dd" format
+
+    var movie = await db.Movies.FindAsync(movieId);
+    if (movie != null)
+    {
+        movie.Title = title;
+        movie.Overview = overview;
+        movie.User_Rating = rating;
+        movie.Language = language;
+        movie.Release_Date = releaseDate; // storing it as a string
+
+        await db.SaveChangesAsync();
+
+        context.Response.Redirect("/adminindex");
+    }
+    else
+    {
+        context.Response.Redirect("/adminindex");
     }
 });
 
